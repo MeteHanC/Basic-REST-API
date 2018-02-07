e18ed83fe1759e4d323a8c9ca707bffc40b97dd8
MeteHanC <metehancet@gmail.com>
2018-02-07 14:42:12 +0200

    1.0

--- API/src/routes.php
+++ API/src/routes.php
@@ -6,11 +6,6 @@ use Slim\Http\Response;
 
 // Routes
 
- $app->get('/', function (Request $request, Response $response, array $args) {
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
- });
-
  $app->get('/get_names', 'StudentController:getStudentNames');
  $app->get("/get_all", "StudentController:getStudents");
  $app->post("/add_student/[{id}[/{name}]]", "StudentController:addStudent");
